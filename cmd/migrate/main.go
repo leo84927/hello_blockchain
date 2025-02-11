6b85433c8b85cd5b737b6c161432d2803982c3ec
Leo <leo@LeodeMacBook-Pro.local>
2025-02-11 15:50:46 +0800

    goimports

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"hello_blockchain/config"
+	"strings"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"hello_blockchain/config"
-	"strings"
 )
 
 func main() {
